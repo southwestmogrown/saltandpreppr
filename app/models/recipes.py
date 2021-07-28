7571c18fda4cc6566ea8ef0b804a7313bf93a92b
southwestmogrown <mail@southwestmogrown.com>
2021-07-28 00:35:49 -0500

    got GET recipes route working

--- app/models/recipes.py
+++ app/models/recipes.py
@@ -7,9 +7,7 @@ class Recipe(db.Model):
     userId = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     type = db.Column(db.String(10))
-    utensils = db.Column(db.Text)
     instructions = db.Column(db.Text, nullable=False)
-    ingredients = db.relationship('Ingredient', back_populates='recipe')
     
 
     def to_dict(self):
@@ -18,7 +16,5 @@ class Recipe(db.Model):
             'userId': self.userId,
             'name': self.name,
             'type': self.type,
-            'utensils': self.utensils,
-            'ingredients': self.ingredients,
             'instructions': self.instructions
         }
