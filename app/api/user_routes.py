df6dfea34418b0bf08d0e9c9ce8adfa7ee3508ae
southwestmogrown <mail@southwestmogrown.com>
2021-07-28 00:49:08 -0500

    got POST route working for recipes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import db, User, Recipe
 
@@ -25,5 +25,16 @@ def get_all_recipes(userId):
 @user_routes.route('/<int:id>/recipes')
 # @login_required
 def recipes(id):
+    recipes = get_all_recipes(id)
+    return {'recipes': [recipe.to_dict() for recipe in recipes]}
+
+@user_routes.route('/<int:id>/recipes', methods=['POST'])
+# @login_required
+def add_recipe(id):
+    data = request.get_json()
+
+    new_recipe = Recipe(userId=data['userId'], name=data['name'], type=data['type'], instructions=data['instructions'])
+    db.session.add(new_recipe)
+    db.session.commit()
     recipes = get_all_recipes(id)
     return {'recipes': [recipe.to_dict() for recipe in recipes]}
